f6acca5948d07c23e7f4eab6c8bc8891542ef7c0
chanamor <teepakorn.san@ku.th>
2025-07-03 18:07:01 +0700

    Add files via upload

--- lib/PasswordValidator.java
+++ lib/PasswordValidator.java
@@ -5,15 +5,62 @@ public class PasswordValidator {
     /**
      * คุณจะต้องเขียน Javadoc ที่สมบูรณ์ที่นี่ในอนาคต
      * เพื่ออธิบายกฎการทำงานของเมธอด
+     * @param password string ที่ต้องการตรวจสอบ
+     * @return ระดับความปลอดภัยของรหัสผ่าน
      */
     // TODO: แก้ไข return type ของเมธอดนี้ให้ถูกต้อง
-    public static void validate(String password) { // Function Type ให้เป็น PasswordStrength 
+    public static PasswordStrength validate(String password) { // Function Type ให้เป็น PasswordStrength 
         
         // ส่วนของ Implementation ที่คุณต้องเขียน
         // ...
         
-        int minLength = 8 // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
+        int minLength = 8; // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
+        if (password.length() <= minLength || password.isEmpty()) {
+            return PasswordStrength.INVALID;
+        }
+
+        boolean UpperCase = false;
+        boolean Lowercase = false;
+        boolean Special = false;
+        boolean Digit = false;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                UpperCase = true;
+            }else if (Character.isLowerCase(c)) {
+                Lowercase = true;
+            }else if (Character.isDigit(c)) {
+                Digit = true;
+            }else
+             Special = true;
+                
+            
+        }
+
+        int passwordstg = 0;
+        if (password.length() >= minLength) {passwordstg++;}
+        if (Digit) {passwordstg++;}
+        if (UpperCase) {passwordstg++;}
+        if (Lowercase) {passwordstg++;}
+        if (Special) {passwordstg++;}
+
+        if (passwordstg >= 4 ) {
+            return PasswordStrength.STRONG;
+        }else if (passwordstg >= 3) {
+            return PasswordStrength.MEDIUM;
+        }else if (passwordstg >= 2) {
+            return PasswordStrength.WEAK;
+        }else{
+            return PasswordStrength.INVALID;
+        }
+            
         
-        return null ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
+        
+        
+
+
+
+
+    
     }
 }
--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -6,15 +6,58 @@ public class PasswordValidatorTest{
         System.out.println("--- Running Password Validator Tests ---");
         
         // Test Case 1: รหัสผ่านสั้นควรจะ INVALID
-        PasswordStrength result1 = PasswordValidator.validate("123");
+        PasswordStrength result1 = PasswordValidator.validate("");
         if (result1 == PasswordStrength.INVALID) {
-            System.out.println("Test Case 1 Passed: Short password is INVALID.");
+            System.out.println("Test Case 1 Passed: Empty password is INVALID.");
         } else {
             System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
         }
 
-        // --- เขียน Test Case อื่นๆ ต่อ ---
+        // Test Case 2: ไม่มีรหัสผ่านควรจะ INVALID
+        PasswordStrength result2 = PasswordValidator.validate("123");
+        if (result2 == PasswordStrength.INVALID) {
+            System.out.println("Test Case 2 Passed: Short password is INVALID.");
+        } else {
+            System.out.println("Test Case 2 FAILED: Expected INVALID but got "+ result2);
+        }
 
-        System.out.println("--------------------------------");
+        // Test Case 3: รหัสผ่านตัวเลขย่างเดียวควร WEAK
+        PasswordStrength result3 = PasswordValidator.validate("12345678910");
+        if (result3 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 3 Passe: Number password is WEAK ");
+        } else {
+            System.out.println("Test Case 3 FAILED: Expected WEAK but got "+ result3);
+        }
+        // Test Case 4: All Lower Case
+        PasswordStrength result4 = PasswordValidator.validate("abcdefghijk");
+        if (result4 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 4 Passe: Lower password is WEAK ");
+        } else {
+            System.out.println("Test Case 4 FAILED: Expected WEAK but got "+ result4);
+        }
+        // Test Case 5: Any UpperCase
+        PasswordStrength result5 = PasswordValidator.validate("Acdefghijk");
+        if (result5 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 5 Passe: Lower password is MEDIUM ");
+        } else {
+            System.out.println("Test Case 5 FAILED: Expected MEDIUM but got "+ result5);
+        }
+        // Test Case 6: มีเลข ตัวอักษร
+        PasswordStrength result6 = PasswordValidator.validate("asdsdsdsdw7894");
+        if (result6 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 6 Passe:มีเลขตัวอักษร password is MEDIUM ");
+        } else {
+            System.out.println("Test Case 5 FAILED: Expected MEDIUM but got "+ result6);
+        }
+        // Test Case 7: รหัสผ่านมีทุกอย่าง
+        PasswordStrength result7 = PasswordValidator.validate("Theamzaza_123456@");
+            if (result6 == PasswordStrength.STRONG) {
+                System.out.println("Test Case 7 Passe: All element is STRONG");
+            } else {
+                System.out.println("Test Case 7 FAILED: Expexted STRONG but got "+result7);
+            }
+
+
+            System.out.println("--------------------------------");
     }
 }
